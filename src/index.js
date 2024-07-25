09e4a4d5c45035a6a07c17e1a1536741c2db0ae6
tatiana <tatkagore@gmail.com>
2024-07-25 15:15:33 +0200

    Fix homepage

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+const basename = document.querySelector("base").getAttribute("href") || "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
